06d8088c1a51ab99f0cdd7d308992913a974bc77
FrancDev <114122165+FrancDev@users.noreply.github.com>
2023-02-07 17:27:08 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿/*Tipos Usando los tipos de variables más adecuados, describe los objetos siguientes:
+Coche: puertas, ruedas, marca, ITV vigente
+Mesa: peso, largo, material, color*/
+
+Console.WriteLine("--COCHE---");
+int puertas = 4;
+int  ruedas = 4;
+string marca = "Ford";
+char ITV = 'S';
+
+Console.WriteLine(" Cantidad de puertas:" +puertas+ "\n Cantidad de ruedas:"+ruedas+ "\n Marca:"+marca+
+                  "\n ITV Vigente?:"+ITV);
+
+Console.WriteLine("--MESA---");
+
+
+
+
+
+
